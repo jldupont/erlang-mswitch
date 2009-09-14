9be40da7ad9b1d68fb965de453398ed4505358e4
jeanlou.dupont <jeanlou.dupont@8d5b2d2e-8998-11de-8f56-7b2e0289298d>
2009-09-14 20:20:29 +0000

    release 2.8

---
+++ project/packages/debian/usr/lib/erlang/lib/mswitch-2.8/src/mswitch_clt.erl
@@ -0,0 +1,94 @@
+%% Author: Jean-Lou Dupont
+%% Created: 2009-09-05
+%% Description: Command-line tools
+%%
+%%
+-module(mswitch_clt).
+-compile(export_all).
+
+
+%% @doc Parses a command line argument list.
+%%		The parameter 'C' denotes a char() which
+%%		serves as 'option' list start.
+%%
+%% @spec parse(C, Arguments) -> ParsedList | {error, Reason}
+%% where
+%%	C = char()
+%%	Arguments = atom() | [term()] | tuple()
+%%	ParsedList = [Item]
+%%	Item = {term, term()} | {Option, [term()]}
+%%
+parse(C, List) when is_list(List) ->
+	do_parse(C, List, []);
+
+parse(_C, Atom) when is_atom(Atom) ->
+	[Atom];
+
+parse(_C, Tuple) when is_tuple(Tuple) ->
+	[Tuple()];
+
+parse(_, []) ->
+	[];
+
+parse(_, Other) ->
+	{error, {unknown_term, Other}}.
+
+
+
+do_parse(_C, [], Acc) ->
+	Acc;
+
+%% @private
+do_parse(C, [Item|Items], Acc) ->
+	case starts_with_char(C, Item) of
+		true ->
+			{List, Rest}=accumulate(C, Item, Items, []),
+			do_parse(C, Rest, Acc++List);
+		false ->
+			do_parse(C, Items, Acc++[{term, Item}])
+	end.
+
+
+	
+%% @doc Verifies if the atom starts with a specific character
+%%
+%% @spec starts_with_char(Char, atom()) -> true | false
+%% where 
+%%	Char = char()
+%%
+starts_with_char(C, E) when is_atom(E) ->
+	[H|_T]=erlang:atom_to_list(E),
+	if
+		H == C ->	true;
+		true   -> 	false
+	end.
+
+accumulate(_, HeadItem, [], Acc) ->
+	{[{HeadItem, Acc}], []};
+
+%% @doc Accumulates Items from the list until
+%%		a) an C is found OR
+%%		b) end of list
+%%
+%% @private
+accumulate(C, HeadItem, [Item|Items], Acc) ->
+	case starts_with_char(C, Item) of
+		true ->	{[{HeadItem, Acc}], [Item|Items]};
+		false -> accumulate(C, HeadItem, Items, Acc++[Item])
+	end.
+
+			
+
+
+	
+%% ----------------------        ------------------------------
+%%%%%%%%%%%%%%%%%%%%%%%%% TESTS  %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+%% ----------------------        ------------------------------
+
+t1() ->
+	L=['#a', a1, a2, '#b', b1, b2, b3],
+	parse($#, L).
+
+t2() ->
+	L2=['w1','w2','#a', a1, a2, '#b', b1, b2, b3],
+	parse($#, L2).
